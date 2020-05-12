7f3b9f11d51ab1c97f57611fad85b1c2b821f8a9
Nascimento, Eduardo (ESI) <eduardo.nascimento@adp.com>
2020-05-12 04:18:18 -0300

    Painel com ações avançadas, faltando apenas os serviços

--- zoom-custom-features.js
+++ zoom-custom-features.js
@@ -1,30 +1,25 @@
 function iniciarEventosDeRotina() {
-    // TODO
+    console.log('iniciarEventosDeRotina() <- função sem conteúdo');
+    /*
+    rotina_validarNomes()
+    rotina_desligarVideosAssistencia();
+    rotina_verificarOpcoesDaSala();
+    */
+}
 
-    // ==== OPCOES AVANCADAS ====
-    // - desligar todos microfones quando focar em alguém (exceto sentinela) (safe)
-    // - desligar vídeo do participante anterior somente quando o atual ligar a câmera
-    // - ligar microfone do participante somente quando ele ligar a câmera
-    // - spotlight automático sempre que o vídeo de quem está em foco for ligado
-    // - ao encerrar o discurso:
-    //     desligar vídeo orador
-    //     desligar microfone orador
-    //     ligar todos microfones
-    //     esperar alguns (talvez 5) segundos de palmas
-    //     desligar todos microfones
-    //     ligar microfone e vídeo do presidente
+function dispararRotina(nomeRotina, tempoEmMilissengudos, callback) {
+    encerrarRotina(nomeRotina);
+    intervalosEmExecucao[nomeRotina] = setInterval(() => callback(), tempoEmMilissengudos);
+}
 
-    // ==== ROTINAS ====
-    // - desligar automaticamente todos vídeos que não sejam participantes (safe)
-    // - verificar nomes fora do padrão e perguntar se quer renomear ou expulsar (safe)
+function encerrarRotina(nomeRotina) {
+    intervalosEmExecucao[nomeRotina] = clearInterval(intervalosEmExecucao[nomeRotina]);
 }
 
 function desenharModal() {
     // limpar componentes anteriores
-    try {
-        document.querySelector('#modal-opcoes-reuniao').remove();
-        document.querySelector('#opcoes-reuniao').remove();
-    } catch { }
+    if (document.querySelector('#modal-opcoes-reuniao')) document.querySelector('#modal-opcoes-reuniao').remove();
+    if (document.querySelector('#opcoes-reuniao')) document.querySelector('#opcoes-reuniao').remove();
 
     var modal = document.createElement('div');
     modal.id = 'modal-opcoes-reuniao';
@@ -58,46 +53,71 @@ function desenharModal() {
 
 function desenharPainelOpcoes() {
     var funcionalidades = [
-        { nome: 'Contar assistência', click: contarAssitencia },
-        { nome: 'Ligar videos', click: ligarVideos, confirmar: 'Tem certeza que deseja LIGAR TODOS OS VIDEOS?' },
-        { nome: 'Desligar videos', click: desligarVideos, confirmar: 'Tem certeza que deseja DESLIGAR TODOS OS VIDEOS?' },
-        { nome: 'Focar no dirigente', click: focarNoDirigente },
-        { nome: 'Focar no leitor', click: focarNoLeitor },
-        // { nome: 'Focar no presidente', click: focarNoPresidente },
-        // { nome: 'Focar no orador', click: focarNoOrador },
-        // { nome: 'Iniciar cântico', click: iniciarCantico, confirmar: 'Tem certeza que deseja INICIAR O CÂNTICO?' },
-        // { nome: 'Finalizar discurso', click: finalizarDiscurso, confirmar: 'Tem certeza que deseja LIGAR TODOS OS VIDEOS?' },
+        { nome: 'Ligar vídeos', icone: 'camera', classe: 'btn-danger', click: ligarVideos, confirmar: 'Tem certeza que deseja LIGAR TODOS OS VÍDEOS?' },
+        { nome: 'Finalizar discurso', icone: 'palmas', classe: 'btn-danger', click: finalizarDiscurso, confirmar: 'Tem certeza que deseja FINALIZAR O DISCURSO? Ligará todos microfones para as palmas.' },
+        { nome: 'Desligar vídeos', icone: 'cameraFechada', classe: 'btn-warning', click: desligarVideos, confirmar: 'Tem certeza que deseja DESLIGAR TODOS OS VIDEOS?' },
+        { nome: 'Iniciar cântico', icone: 'microfoneFechado', classe: 'btn-warning', click: iniciarCantico, confirmar: 'Tem certeza que deseja INICIAR O CÂNTICO? Desligará todos vídeos e microfones.' },
+        { nome: 'Focar no presidente', icone: 'participante', classe: 'btn-primary', click: focarNoPresidente },
+        { nome: 'Focar no dirigente', icone: 'participante', classe: 'btn-primary', click: focarNoDirigente },
+        { nome: 'Focar no leitor', icone: 'participante', classe: 'btn-primary', click: focarNoLeitor },
+        { nome: 'Focar no orador', icone: 'participante', classe: 'btn-primary', click: focarNoOrador },
+        { nome: 'Contar assistência', icone: 'assistencia', classe: 'btn-primary', click: contarAssitencia },
         // TODO: OPCIONAIS
-        // { nome: 'Leitura Bíblia', click: leituraBiblia },
-        // { nome: 'Demonstracao 1', click: demonstracao1 },
-        // { nome: 'Demonstracao 2', click: demonstracao2 },
+        // { nome: 'Leitura Bíblia', icone: 'participante', classe: 'btn-primary', click: leituraBiblia },
+        // { nome: 'Demonstracao 1', icone: 'assistencia', classe: 'btn-primary', click: demonstracao1 },
+        // { nome: 'Demonstracao 2', icone: 'assistencia', classe: 'btn-primary', click: demonstracao2 },
     ];
-
+    var rotinas = [
+        { nome: 'Validar nomes inválidos', classe: 'btn-primary', click: rotina_validarNomes },
+        { nome: 'Verificar opções sala', classe: 'btn-primary', click: rotina_verificarOpcoesDaSala },
+        { nome: 'Desligar vídeos assistência', classe: 'btn-warning', click: rotina_desligarVideosAssistencia },
+    ];
     // construir botoes principais
     var botoes = funcionalidades.map(func => {
         var btn = document.createElement('button');
         btn.innerText = func.nome;
         btn.onclick = () => !func.confirmar ? func.click() : confirm(`CUIDADO!\n\n${func.confirmar}\n\nClique em cancelar para desfazer`) && func.click();
-        btn.setAttribute('class', 'btn btn-primary');
+        btn.setAttribute('class', `btn ${func.classe}`);
         btn.style.cssText = `
-            width: 80%;
+            display: flex;
+            flex-direction: row-reverse;
+            align-items: center;
+            justify-content: space-evenly;
+            width: 95%;
             height: 90%;
             margin: auto;
+            font-size: 20px;
+            opacity: 0.8;
         `;
 
+        if (func.icone) {
+            var icon = criarIcone(func.icone, btoa(func.nome));
+            icon.style.filter = 'brightness(10)';
+            btn.appendChild(icon);
+        }
         return btn;
     });
-
+    var btnFechar = criarIcone('fechar', 'btn-fechar-modal');
+    btnFechar.style.left = '85%';
+    btnFechar.style.top = '1%';
+    btnFechar.style.opacity = '0.7';
+    btnFechar.onclick = fecharModal;
     // construtir o frame com botoes
-    var frame = document.createElement('div');
-    frame.style.cssText = `
+    var frameBotoes = document.createElement('div');
+    frameBotoes.style.cssText = `
         display: grid;
         grid-template-columns: 1fr 1fr;
         grid-template-rows: repeat(6, 1fr);
-        grid-gap: 10px;
-        margin-top: 40px;
+        grid-gap: 5px;
+        padding: 10px;
     `;
-
+    // adicionar os botoes no frame
+    botoes.forEach(btn => frameBotoes.appendChild(btn));
+    // construtir o frame com servicos
+    var frameServicos = document.createElement('div');
+    frameServicos.appendChild(btnFechar);
+    // adicionar os servicos no frame
+    // TODO: criar servicos
     // construir o quadro inteiro do painel
     var painelOpcoes = document.createElement('div');
     painelOpcoes.id = 'opcoes-reuniao';
@@ -106,18 +126,45 @@ function desenharPainelOpcoes() {
         position: fixed;
         top: 5%;
         left: 10%;
-        width: 1000px;
-        height: 550px;
+        width: 1100px;
+        height: 600px;
         grid-template-columns: 2fr 1fr;
         grid-gap: 30px;
-        background-color: white;
+        background-color: #edf2f7e6;
+        border-radius: 10px;
     `;
-
-    // adicionar os botoes no frame
-    botoes.forEach(btn => frame.appendChild(btn));
-    // adicionar o frame com botoes ao painel inteiro
-    painelOpcoes.appendChild(frame);
+    // adicionar os frames ao painel
+    painelOpcoes.appendChild(frameBotoes);
+    painelOpcoes.appendChild(frameServicos);
     return painelOpcoes;
+}
+
+function criarIcone(tipo, id) {
+    if (!tipo || !id) return;
+    var tipos = {
+        camera: 'background-position: 264px 1128px;',
+        cameraFechada: 'background-position: 215px 1127px;',
+        microfone: 'background-position: -193px 553px;',
+        microfoneFechado: 'background-position: -242px 554px;',
+        palmas: 'background-position: 637px 875px;',
+        participante: 'background-position: 400px 1080px; background-size: 574px 645px;',
+        cadeado: 'background-position: 117px 1124px; background-size: 800px 670px;',
+        assistencia: 'background-position: 400px 1080px; background-size: 650px 645px;',
+        fechar: 'background-position: 323px 1200px; background-size: 1200px 959px;',
+    };
+    var icone = document.createElement('i');
+    icone.id = id;
+    icone.style.cssText = `
+        cursor: pointer;
+        position: relative;
+        height: 45px;
+        width: 50px;
+        display: inline-block;
+        background: url(https://us04st1.zoom.us/web_client/pwj3uo/image/wc_sprites.png);
+        background-size: 840px 740px;
+        ${tipos[tipo]}
+    `;
+    return icone;
 }
 
 function abrirModal() {
@@ -211,16 +258,19 @@ function ligarVideoParticipante(participante, callback) {
     if (participante && callback) {
         var nomeParticipante = getNomeParticipante(participante);
 
-        intervalosEmExecucao[nomeParticipante] = setInterval(() => {
+        // iniciar temporizador para aguardar participante liberar video
+        var repeticoes = 0;
+        dispararRotina(nomeParticipante, 500, () => {
+            repeticoes++;
             if (isVideoLigado(participante)) {
-                intervalosEmExecucao[nomeParticipante] = clearInterval(intervalosEmExecucao[nomeParticipante]);
+                encerrarRotina(nomeParticipante);
                 callback();
-            } else {
+            } else if (repeticoes > 5) {
+                // nao tentar ativar o video toda hora para evitar processamento desnecessario
                 console.log(`Loop de rotina [ligar vídeo] --EM EXECUÇÃO-- para: ${nomeParticipante}.`);
                 clickDropdown(participante, textosBotao, mensagemErro);
             }
-
-        }, 500); // tempo em milissegundos
+        });
     }
 }
 
@@ -237,6 +287,38 @@ function spotlightParticipante(participante) {
         ['spotlight video', 'vídeo de destaque'],
         'Não foi possível ACIONAR SPOTLIGHT! Verifique o nome do participante.'
     );
+}
+
+function ligarMicrofones() {
+    var textos = ['mute all', 'unmute all', 'desativar som de todos', 'ativar som de todos'];
+    var textosMuteAll = ['mute all', 'desativar som de todos'];
+    var btn = Array
+        .from(document.querySelectorAll('.participants-section-container__participants-footer button'))
+        .find(btn => textos.includes(btn.innerText.toLowerCase()));
+
+    if (!btn) return;
+
+    // se botao estiver na opcao silenciar todos, entao sera preciso desligar para depois ligar microfones
+    if (textosMuteAll.includes(btn.innerText.toLowerCase())) {
+        // abrir modal para silenciar todos
+        btn.click();
+
+        var btnConfirmarMute = document.querySelector('.zm-modal-footer-default-actions .zm-btn--primary');
+        var checkboxPermitirUnmute = document.querySelector('.zm-modal-footer-default-checkbox .zm-checkbox');
+
+        // desmarcar a opcao 'participantes desativem o mudo'
+        if (checkboxPermitirUnmute) {
+            checkboxPermitirUnmute.setAttribute('aria-checked', 'false');
+            checkboxPermitirUnmute.removeAttribute('class');
+            checkboxPermitirUnmute.setAttribute('class', 'zm-checkbox');
+        }
+
+        // confirmar opcao silenciar todos para que surja a opcao para liberar microfones
+        btnConfirmarMute && btnConfirmarMute.click();
+    }
+
+    // por fim, liberar todos os microfones
+    btn.click();
 }
 
 function desligarMicrofones(execoes) {
@@ -268,7 +350,8 @@ function desligarVideos(execoes) {
     });
 }
 
-// funcoes avancadas
+// FUNCOES AVANCADAS
+
 function focarNoDirigente() {
     abrirPainelParticipantes();
     var dirigente = selecionarParticipante('dirigente');
@@ -310,9 +393,60 @@ function focarNoLeitor() {
     });
 }
 
-function focarNoPresidente() { /* TODO */ }
+function focarNoPresidente() {
+    abrirPainelParticipantes();
+    var presidente = selecionarParticipante('presidente');
 
-function focarNoOrador() { /* TODO */ }
+    // silenciar todos participantes, exceto presidente
+    desligarMicrofones([presidente]);
+
+    // ligar video do presidente
+    ligarVideoParticipante(presidente, () => {
+        // quando o presidente iniciar seu video
+        ligarMicrofoneParticipante(presidente);
+        spotlightParticipante(presidente);
+        desligarVideos([presidente]); // exceto presidente
+    });
+}
+
+function focarNoOrador() {
+    abrirPainelParticipantes();
+    var orador = selecionarParticipante('orador');
+
+    // desligar video de todos participantes, exceto orador
+    desligarVideos([orador]);
+
+    // silenciar todos participantes, exceto orador
+    desligarMicrofones([orador]);
+
+    // ligar video do orador
+    ligarVideoParticipante(orador, () => {
+        // quando o orador iniciar seu video
+        spotlightParticipante(orador);
+        ligarMicrofoneParticipante(orador);
+    });
+}
+
+function finalizarDiscurso() {
+    abrirPainelParticipantes();
+    var presidente = selecionarParticipante('presidente');
+
+    // desligar video de todos participantes
+    desligarVideos();
+
+    // ligar microfone de todos participantes para as palmas
+    ligarMicrofones();
+
+    // Aguardar tempo suficiente de palmas
+    setTimeout(() => {
+        // ligar video do presidente
+        ligarVideoParticipante(presidente, () => {
+            // quando o presidente iniciar seu video
+            spotlightParticipante(presidente);
+            ligarMicrofoneParticipante(presidente);
+        });
+    }, 8000);
+}
 
 // somente desliga videos e microfones
 function iniciarCantico() {
@@ -332,8 +466,40 @@ function contarAssitencia() {
         };
     });
 
-    alert(`Contagem automatizada da assistência: ${assistencia}`);
+    alert(`Contagem automatizada da assistência: ${assistencia}\nContando apenas nomes no padrão: (1) Nome `);
 }
+
+// ROTINAS DE LOOP
+
+function rotina_desligarVideosAssistencia() {
+    var rotina = 'validarVideosLigadosNaAssistencia';
+    // iniciar rotina de 10 segundos
+    dispararRotina(rotina, 10000, () => {
+        // Atualizar sempre que uma funcao nova for mapeada
+        desligarVideos([
+            // desligar todos videos, com excecao dos participantes abaixo
+            selecionarParticipante('dirigente'),
+            selecionarParticipante('leitor'),
+            selecionarParticipante('presidente'),
+            selecionarParticipante('orador'),
+        ]);
+    });
+}
+
+function rotina_validarNomes() {
+    var rotina = 'validarNomesForaDoPadrao';
+    // iniciar rotina de 15 segundos
+    dispararRotina(rotina, 15000, () => {
+        var invalidos = getParticipantes().filter(participante => !/\s*[\(\[\{]\s*[0-9]/ig.test(getNomeParticipante(participante)));
+
+        if (invalidos) {
+            alert(`participantes com nome inválido:\n${invalidos.join('\n')}`);
+        }
+    });
+}
+
+// verificar se participantes podem se desmutar, renomear, entram silenciados, se a sala está trancada...
+function rotina_verificarOpcoesDaSala() { /* TODO */ }
 
 // INICIO DO SCRIPT
 desenharModal();
@@ -345,8 +511,12 @@ if (!document.querySelector('#abrir-opcoes-reuniao')) {
     var btnAbrirModal = document.createElement('button');
     btnAbrirModal.id = 'abrir-opcoes-reuniao';
     btnAbrirModal.innerText = 'Opções customizadas';
-    btnAbrirModal.style.marginRight = '10px';
+    btnAbrirModal.style.marginRight = '20px';
     btnAbrirModal.onclick = abrirModal;
     // adicionar botao no rodape
     document.querySelector('#wc-footer').appendChild(btnAbrirModal);
 }
+
+// TODO: MELHORIAS
+// desligar video do participante anterior somente quando o atual ligar a camera
+// antes de contar a assistencia, validar os nomes invalidos
