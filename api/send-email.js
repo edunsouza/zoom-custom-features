b9a31c0b1549e640ee5ea2e13c4c2d7d11f8eb24
Nascimento, Eduardo (ESI) <eduardo.nascimento@adp.com>
2020-07-26 04:10:22 -0300

    [master] - Fix moment locale issue in send-email serverless function

--- api/send-email.js
+++ api/send-email.js
@@ -1,5 +1,6 @@
 const sgMail = require('@sendgrid/mail');
 const moment = require('moment');
+require('moment/locale/pt-br');
 
 const getHtmlBody = (assistencia = 'Não informado', congregacao = 'Nordeste', reuniao) => {
     return `
@@ -7,7 +8,7 @@ const getHtmlBody = (assistencia = 'Não informado', congregacao = 'Nordeste', r
             <h1 style="text-align: center; background-color: #4a6ca7; color: white; text-transform: uppercase;">
                 ${congregacao} - ${reuniao}
             </h1>
-            <h1 style="text-align: center; color: #4a6ca7">Assitência: ${assistencia}</h1>
+            <h1 style="text-align: center; color: #4a6ca7">Assistência: ${assistencia}</h1>
             <img
                 src="https://assetsnffrgf-a.akamaihd.net/assets/m/802013048/univ/art/802013048_univ_sqr_md.jpg"
                 style="display: block; margin-left: auto; margin-right: auto;"
@@ -36,20 +37,23 @@ const sendEmail = async (req, res) => {
         const { assistencia, congregacao } = req.body;
         const reuniao = moment.locale('pt-br') && moment().format('DD/MM/YYYY - dddd');
 
+        if (!assistencia || assistencia < 1) {
+            return res.status(500).json({ success: false, message: 'Assistência não informada!' });
+        }
+
         sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-        const msg = {
+
+        await sgMail.send({
             from: `Automação - Nordeste <${process.env.EMAIL_SENDER}>`,
             to: (process.env.EMAIL_TO || '').split(','),
             subject: `Assistência Nordeste - ${reuniao}`,
             html: getHtmlBody(assistencia, congregacao, reuniao)
-        };
-        await sgMail.send(msg);
+        });
 
-        res.status(200).json({ success: true, message: 'E-mail enviado com sucesso!', body: req.body });
+        res.status(200).json({ success: true, message: 'E-mail enviado com sucesso!' });
     } catch (error) {
-        console.log(error);
-        error.response && console.log(error.response.body);
-        res.status(500).json({ success: false, message: 'Dados incorretos!', body: req.body });
+        console.log(JSON.stringify(error, null, 4));
+        res.status(500).json({ success: false, message: 'Dados incorretos!' });
     }
 }
 
