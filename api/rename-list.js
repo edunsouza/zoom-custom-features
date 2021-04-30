76203ddd65480c77bbcc77aac026b8db49ce6183
Eduardo Souza <eduardo.souza.ens@gmail.com>
2021-04-30 00:40:12 -0300

    [master] - first version o rename-list api

---
+++ api/rename-list.js
@@ -0,0 +1,40 @@
+const { google } = require('googleapis');
+const { allowCors } = require('../helpers');
+
+let sheetsAPI = null;
+const keyFile = 'secret/index.json';
+const scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+
+async function getSheet(spreadsheetId, range) {
+	if (sheetsAPI === null) {
+		sheetsAPI = google.sheets({
+			version: 'v4',
+			auth: new google.auth.GoogleAuth({ keyFile, scopes })
+		});
+	}
+
+	return await sheetsAPI.spreadsheets.values.get({ spreadsheetId, range });
+}
+
+async function getRenameList(req, res) {
+	try {
+		const { id } = req.query || {};
+
+		if (!id) {
+			return res.status(400).json({ success: false, message: 'Não encontrado!' });
+		}
+
+		const sheet = await getSheet('1z8whRV9lIIPutRN8zxNrGo5rG6uLEulM96H6VCyvAJM', 'key-value!A2:B');
+		const row = sheet.data.values.find(r => r.includes(id));
+		const renameListId = row.pop();
+
+		const renameList = await getSheet(renameListId, 'zoom!A2:B');
+
+		res.status(200).json({ success: true, list: renameList.data.values });
+	} catch (error) {
+		console.log(JSON.stringify(error, null, 4));
+		res.status(500).json({ success: false, message: 'Dados incorretos!' });
+	}
+}
+
+module.exports = allowCors(getRenameList);
